587015ec269b35fc037b3bd7ee927e90a832a58d
Kazuki <kazuki_99@outlook.com>
2020-12-08 11:37:47 +0800

    完成plusOne

---
+++ Week02/算法/plusOne.js
@@ -0,0 +1,19 @@
+const arr = [0, 9, 2]
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+var plusOne = function(digits) {
+    const len = digits.length;
+    for(let i = len - 1; i >= 0; i--) {
+        digits[i]++;
+        digits[i] %= 10;
+        if(digits[i]!=0)
+            return digits;
+    }
+    digits.unshift(1)
+    return digits
+};
+
+console.log(plusOne(arr));
