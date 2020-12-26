1fb29e08cf7565462195793e43173d140c0f26c9
Kazuki <kazuki_99@outlook.com>
2020-12-26 18:04:02 +0800

    完成KMP

--- Week04/KMP.js
+++ Week04/KMP.js
@@ -1,12 +1,52 @@
 // abcdabce
 function KMP(source, pattern) {
+	let time = 0;
 	// 计算table
+	let pre_table = new Array(pattern.length).fill(0);
 
-
-
+	// len 指的是pattern最长前后缀的长度
+	let len = 0,
+		i = 1;
+	while (i < pattern.length) {
+		time++;
+		if (pattern[i] === pattern[len]) {
+			++len, ++i;
+			pre_table[i] = len;
+		} else {
+			if (len > 0) {
+				len = pre_table[len];
+			} else {
+				if (len < 0) {
+					len = 0;
+				}
+				++i;
+			}
+		}
+	}
+	console.log(pre_table);
 
 	// 匹配
+	let sIndex = 0;
+	let pIndex = 0;
+	while (sIndex < source.length) {
+		time++;
+		if (source[sIndex] === pattern[pIndex]) {
+			++pIndex, ++sIndex;
+			if (pattern.length === pIndex) {
 
+				console.log(time, source.length, pattern.length);
+				return true;
+			}
+		} else {
+			if (pIndex === 0) {
+				++sIndex;
+				continue;
+			}
+			pIndex = pre_table[pIndex];
+		}
+	}
+
+	console.log(time, source.length, pattern.length);
 	return [source, pre_table];
 }
-console.log(KMP('', 'abcdabce'));
+console.log(KMP('lkasjdflkjasdkfjlaskdfkjabdacccccccebcd', 'ccc'));
